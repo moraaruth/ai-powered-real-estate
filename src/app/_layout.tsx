ef7cc5477da09906fa96913dbb40de60cc899f7b
Ruth Moraa Nyangau <RMNYANGAU@safaricom.co.ke>
2026-06-28 08:05:24 +0300

    adding queryclient

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,25 @@
-import "../global.css"
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import { useColorScheme } from 'react-native';
+import "../global.css";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+import { DarkTheme, DefaultTheme, ThemeProvider } from "expo-router";
+import { useColorScheme } from "react-native";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { AnimatedSplashOverlay } from "@/components/animated-icon";
+import AppTabs from "@/components/app-tabs";
+
+// create query client once (outside component is best practice)
+const queryClient = new QueryClient();
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <AnimatedSplashOverlay />
+        <AppTabs />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
